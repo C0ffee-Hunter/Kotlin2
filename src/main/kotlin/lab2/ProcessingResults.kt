9dc74201ac2b7dbe7774bc2c69a324f2ad8f5176
Tribule <74187676+KingOfCamelot@users.noreply.github.com>
2022-11-24 18:35:36 +0300

    some fix

--- src/main/kotlin/lab2/ProcessingResults.kt
+++ src/main/kotlin/lab2/ProcessingResults.kt
@@ -1,1 +1,38 @@
+package lab2
 
+class ProcessingResults(private val data: HashMap<Address, Int>) {
+    fun printCopy()
+    {
+        data.forEach {
+            println("${it.key} повторений ${it.value}")
+        }
+    }
+
+    fun counterFloor()
+    {
+        val countFloor = hashMapOf<String, CountFloor>()
+        for (address in data)
+        {
+            if (!countFloor.containsKey(address.key.city)) {
+                countFloor[address.key.city] = CountFloor()
+            }
+            val tempForCounter = 1
+            when (address.key.floor) {
+                1 -> countFloor[address.key.city]?.one = countFloor[address.key.city]?.one!! + tempForCounter
+                2 -> countFloor[address.key.city]?.two = countFloor[address.key.city]?.two!! + tempForCounter
+                3 -> countFloor[address.key.city]?.three = countFloor[address.key.city]?.three!! + tempForCounter
+                4 -> countFloor[address.key.city]?.four = countFloor[address.key.city]?.four!! + tempForCounter
+                5 -> countFloor[address.key.city]?.five = countFloor[address.key.city]?.five!! + tempForCounter
+            }
+        }
+        countFloor.forEach {
+            println("\nКоличество домов в городе ${it.key}")
+            println("Одноэтажных домов: ${it.value.one}")
+            println("Двухэтажных домов: ${it.value.two}")
+            println("Трёхэтажных домов: ${it.value.three}")
+            println("Четырёхэтажных домов: ${it.value.four}")
+            println("Пятиэтажных домов: ${it.value.five}")
+        }
+
+    }
+}
