ff00512345212ee83faad08a679f802d591d538f
Tribule <74187676+KingOfCamelot@users.noreply.github.com>
2022-11-24 18:27:37 +0300

    add main file for work programm

--- src/main/kotlin/lab2/Main.kt
+++ src/main/kotlin/lab2/Main.kt
@@ -1,1 +1,24 @@
+package lab2
 
+fun main()
+{
+    do
+    {
+        print("Введите путь до нужного файла или выйдете из программы введя - 2: ")    //C://address.csv
+        var wayToFile = readln()         //C://address.xml
+        if (wayToFile[wayToFile.length - 1] == '2')
+            wayToFile = "2"
+        if (wayToFile != "2")
+        {
+            println("НАЧИНАЕМ!!!!")
+            val start = System.currentTimeMillis()
+            val data = ReadFile(wayToFile)
+            val process = ProcessingResults(data.listData)
+            process.printDuplicate()
+            process.cityFloor()
+            val end = System.currentTimeMillis()
+            print("Было затрачено времени: ")
+            println((end - start) / 1000)
+        }
+    } while (wayToFile != "2")
+}
